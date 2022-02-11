9e3b66023d4b5b32a1e6cb6b70cb52c22b274bac
Razu <razuahmed8641@gmail.com>
2022-02-12 02:05:10 +0600

    dropDown with hint text

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,79 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace, prefer_const_literals_to_create_immutables, non_constant_identifier_names
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter AppDevelopment+',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: "Flutter Demo",
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class _HomePageState extends State<HomePage> {
+  var _selectedValues;
+  List<dynamic> Country = <String>[
+    "Bangladesh",
+    "India",
+    "Canada",
+    "Japan",
+    "China",
+  ];
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.blue,
+    ));
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Floating Action Button"),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        leading: Icon(Icons.menu),
+        actions: const [
+          Icon(Icons.more_vert),
+        ],
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: SizedBox(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              DropdownButton(
+                value: _selectedValues,
+                hint: Text("Select Country"),
+                items: [
+                  for (var i = 0; i < Country.length; i++)
+                    DropdownMenuItem(
+                      value: i,
+                      child: Text(Country[i]),
+                    ),
+                ],
+                onChanged: (e) {
+                  setState(() {});
+                  _selectedValues = e;
+                },
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
